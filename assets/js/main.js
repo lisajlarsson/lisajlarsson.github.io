99f060815e71e58679e2b31fd6d7129be30a1dc2
Rickard Rosén <rickard.rosen@tui.se>
2017-12-16 15:28:31 +0100

    index.md & 404

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 window.sr = ScrollReveal();
 sr.reveal('.headline');
 sr.reveal('.featured-item', {viewFactor: 0.01});
-
+sr.reveal('.about-section', {viewFactor: 1.00});
 var $root = $('html, body');
 
 $('a[href^="#"]').click(function () {
